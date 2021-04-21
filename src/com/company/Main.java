3795d93222649ac534e9f4cb1cd946ea1033d5ca
dankcf <3355137186@qq.com>
2021-04-21 09:36:17 +0800

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+<<<<<<< HEAD
 	// write your code here
+=======
+        System.out.println("wdnmd");
+
+>>>>>>> Initial commit
     }
 }
